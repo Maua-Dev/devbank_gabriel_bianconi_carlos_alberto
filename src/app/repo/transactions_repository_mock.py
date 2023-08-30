25047dd268e6b8d7b8274f73138786c92f9deb03
GabrielBianconiconi <gabrielbianconiconi@gmail.com>
2023-08-30 16:18:58 -0300

    fixing transactions😎

--- src/app/repo/transactions_repository_mock.py
+++ src/app/repo/transactions_repository_mock.py
@@ -9,8 +9,11 @@ class TransactionRepositoryMock(ITransactionsRepository):
     
     def __init__(self):
         self.transactions_list = [
-             Transaction(TRANSACTIONTYPEENUM.WITHDRAW, 100.00, 150.00, time.time()),
-             Transaction(TRANSACTIONTYPEENUM.DEPOSIT, 100.00, 200.00, time.time()),
+             Transaction(TRANSACTIONTYPEENUM.DEPOSIT, 100.00, 1000.00, time.time()),
+             Transaction(TRANSACTIONTYPEENUM.WITHDRAW, 300.00, 700.00, time.time()),
+             Transaction(TRANSACTIONTYPEENUM.DEPOSIT, 10.00, 710.00, time.time()),
+             Transaction(TRANSACTIONTYPEENUM.WITHDRAW, 30.00, 680.00, time.time()),
+             
         ]
         
     def get_all_transactions(self) -> List[Transaction]:
